3610a853361e0b208c4fae9bad929b748a162794
pioneersingh321 <pioneersingh321@gmail.com>
2026-09-22 09:20:37 +0530

    feat: add Android build configuration and FileUploaderPlugin

--- android/src/main/java/bolt/fileuploader/capacitor/FileUploaderPlugin.java
+++ android/src/main/java/bolt/fileuploader/capacitor/FileUploaderPlugin.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 
-import com.getcapacitor.ActivityResult;
+import androidx.activity.result.ActivityResult;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.PermissionState;
